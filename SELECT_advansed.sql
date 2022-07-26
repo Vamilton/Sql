45cc280299895a50653e0af06082d511d0dcc993
Vamilton <arteiano@yandex.ru>
2022-07-26 16:55:55 +0300

    Commit it

--- SELECT_advansed.sql
+++ SELECT_advansed.sql
@@ -12,30 +12,28 @@ join albums a on a.id = t.albumid
 group by a.name;
 
 select a.name from artist a
+	where a.name not in (
+select a.name from artist a
 inner join artist_albums aa on a.id = aa.artistid
 inner join albums a2 on aa.albumid = a2.id
-where a2.year != 2020;
+where a2.year = 2020);
 
-select c.name from collection c
+select DISTINCT c.name from collection c
 inner join collection_list cl on c.id = cl.collid
 inner join track t on cl.trackid = t.id
 inner join albums a on t.albumid = a.id
 inner join artist_albums aa on a.id = aa.albumid
 inner join artist a2 on aa.artistid = a2.id
 where a2.name like 'Reach'
-group by c.name
 
 
-WITH numberInfo AS(
-select a.name, count(g.name) as ccc from albums a
+select a.name from albums a
 inner join artist_albums aa on a.id = aa.albumid
 inner join artist a2 on aa.artistid = a2.id
 inner join artist_genre ag on a2.id = ag.artistid
 inner join genre g on ag.genreid = g.id
 group by a.name
-)
-select name FROM numberInfo
-where ccc > 1;
+having count(g.name) > 1;
 
 select t.name from track t
 left join collection_list cl on t.id = cl.trackid
@@ -43,15 +41,12 @@ left join collection c on cl.collid = c.id
 where c.name is null;
 
 
-WITH timetime as(
-select a.name, t.time from artist a
+select a.name from artist a
 inner join artist_albums aa on a.id = aa.artistid
 inner join albums a2 on aa.albumid = a2.id
 inner join track t on a2.id = t.albumid
-)
-select name from timetime
-	where time = (SELECT min(time) FROM timetime)
-group by name;
+where t.time = (SELECT min(time) FROM track)
+group by a.name;
 
 
 with howmany as (
